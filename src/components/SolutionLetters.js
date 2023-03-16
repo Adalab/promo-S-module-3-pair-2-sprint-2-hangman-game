2f0903c09633355ed942eeb6c1fef87cace5db65
Maria <araujomaria84@gmail.com>
2023-03-16 11:06:42 +0100

    Add componets y props exercise

---
+++ src/components/SolutionLetters.js
@@ -0,0 +1,24 @@
+import "../styles/Letters.scss";
+
+function SolutionLetters ({userLetters, word}){
+
+    const renderSolutionLetters = () => {
+    const wordLetters = word.split("");
+    return wordLetters.map((letter, index) => {
+      const exists = userLetters.includes(letter.toLocaleLowerCase());
+      return (
+        <li key={index} className='letter'>
+          {exists ? letter : ""}
+        </li>
+      );
+    });
+  };
+    return (
+        <div className='solution'>
+            <h2 className='title'>Solución:</h2>
+            <ul className='letters'>{renderSolutionLetters()}</ul>
+          </div>
+
+    )
+}
+export default SolutionLetters;
